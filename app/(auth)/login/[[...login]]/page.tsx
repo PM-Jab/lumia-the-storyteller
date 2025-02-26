3d6bcce83d909c92ce1b087008a539b51f45ac75
Possawee Muttisan <possawee.m@arise.tech>
2025-02-26 16:23:31 +0700

    comment clerk lib

--- app/(auth)/login/[[...login]]/page.tsx
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "../../../../components/Hero";
+// import Hero from "../../../../components/Hero";
 
-export default function Login() {
-  return (
-    <div className="bg-[#f0ead2] w-full h-screen">
-      <Hero />
-    </div>
-  );
-}
+// export default function Login() {
+//   return (
+//     <div className="bg-[#f0ead2] w-full h-screen">
+//       <Hero />
+//     </div>
+//   );
+// }
