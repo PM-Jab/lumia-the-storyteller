ab5b95481c15f5b17c224386f9cb8d920a1e33c2
Possawee Muttisan <possawee.m@arise.tech>
2025-02-26 16:29:19 +0700

    fix: add something on (auth) pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,15 @@
 // import { SignIn } from "@clerk/nextjs";
-// import Image from "next/image";
+import Image from "next/image";
 
-// export default function Page() {
-//   return (
-//     <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
-//       <div className="h-screen w-full relative">
-//         <Image src={"/login.png"} alt="login" layout="fill" objectFit="cover" />
-//       </div>
-//       <div className="flex justify-center items-center h-screen order-first md:order-last">
-//         <SignIn />
-//       </div>
-//     </div>
-//   );
-// }
+export default function Page() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
+      <div className="h-screen w-full relative">
+        <Image src={"/login.png"} alt="login" layout="fill" objectFit="cover" />
+      </div>
+      <div className="flex justify-center items-center h-screen order-first md:order-last">
+        {/* <SignIn /> */}
+      </div>
+    </div>
+  );
+}
