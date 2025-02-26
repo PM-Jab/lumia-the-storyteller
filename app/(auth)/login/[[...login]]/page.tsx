e4f7161919fb7c10cc60cdacc9b370e71f5713d1
Possawee Muttisan <possawee.m@arise.tech>
2025-02-26 16:41:42 +0700

    feat: remove auth

--- app/(auth)/login/[[...login]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "../../../../components/Hero";
-
-export default function Login() {
-  return (
-    <div className="bg-[#f0ead2] w-full h-screen">
-      <Hero />
-    </div>
-  );
-}
